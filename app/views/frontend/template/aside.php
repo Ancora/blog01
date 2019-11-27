9449a69d7881cb0435d97d290f935d754f1abf99
Ancora <anderson.rocha@ancora-ti.com.br>
2019-11-27 16:45:40 -0300

    Limpar URL, templates e prim model.

---
+++ app/views/frontend/template/aside.php
@@ -0,0 +1,36 @@
+<!-- Blog Sidebar Widgets Column -->
+            <div class="col-md-4">
+
+                <!-- Blog Search Well -->
+                <div class="well">
+                    <h4>Pesquisar no Blog</h4>
+                    <div class="input-group">
+                        <input type="text" class="form-control">
+                        <span class="input-group-btn">
+                            <button class="btn btn-default" type="button">
+                                <span class="glyphicon glyphicon-search"></span>
+                        </button>
+                        </span>
+                    </div>
+                    <!-- /.input-group -->
+                </div>
+
+                <!-- Blog Categories Well -->
+                <div class="well">
+                    <h4>Categorias do Blog</h4>
+                    <div class="row">
+                        <div class="col-lg-12">
+                            <ul class="list-unstyled">
+                                <li><a href="#">Modelos</a>
+                                </li>
+                                <li><a href="#">Eventos</a>
+                                </li>
+                                <li><a href="#">Natureza</a>
+                                </li>
+                                <li><a href="#">Minhas Viagens</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                    <!-- /.row -->
+                </div>
