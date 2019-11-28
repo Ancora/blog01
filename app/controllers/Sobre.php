bf23f14f04c8985db94bbccbf62e15b605982513
Ancora <anderson.rocha@ancora-ti.com.br>
2019-11-27 23:47:13 -0300

    Pages Postagens e Autores.

---
+++ app/controllers/Sobre.php
@@ -0,0 +1,48 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Sobre extends CI_Controller {
+
+	public function __construct() {
+		parent::__construct();
+
+		$this->load->model('categorias_model', 'modelcategorias');
+		$this->categorias = $this->modelcategorias->listar_categorias();
+	}
+
+	public function index($id, $slug=null)
+	{
+		$dados['categorias'] = $this->categorias;
+		$this->load->model('postagens_model', 'modelpostagens');
+		$dados['postagens'] = $this->modelpostagens->categoria_post($id);
+
+		/* Dados para envio ao Header */
+		$dados['titulo'] = 'Categorias';
+		$dados['subtitulo'] = '';
+		$dados['subtitulodb'] = $this->modelcategorias->listar_titulo($id);
+
+		$this->load->view('frontend/template/html-header', $dados);
+		$this->load->view('frontend/template/header');
+		$this->load->view('frontend/categoria');
+		$this->load->view('frontend/template/aside');
+		$this->load->view('frontend/template/footer');
+		$this->load->view('frontend/template/html-footer');
+	}
+
+	public function autores($id, $slug=null) {
+		$dados['categorias'] = $this->categorias;
+		$this->load->model('usuarios_model', 'modelusuarios');
+		$dados['autores'] = $this->modelusuarios->listar_autor($id);
+
+		/* Dados para envio ao Header */
+		$dados['titulo'] = 'Sobre';
+		$dados['subtitulo'] = 'Autor';
+
+		$this->load->view('frontend/template/html-header', $dados);
+		$this->load->view('frontend/template/header');
+		$this->load->view('frontend/autor');
+		$this->load->view('frontend/template/aside');
+		$this->load->view('frontend/template/footer');
+		$this->load->view('frontend/template/html-footer');
+	}
+}
